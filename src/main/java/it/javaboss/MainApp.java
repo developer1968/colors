21b5955a3aef2c4e0a005462daef2d1d0a04f1ef
svil1 <aviggiani@schema31.it>
2020-10-23 15:41:00 +0200

    cambi cambi

--- src/main/java/it/javaboss/MainApp.java
+++ src/main/java/it/javaboss/MainApp.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainApp extends SpringBootServletInitializer{
+public class MainApp {
    public static void main(String[] args) {
       
 	   SpringApplication.run(MainApp.class, args);
