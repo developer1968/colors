738998b82bd23e75ddc1195c9cc611c6915370a1
svil1 <aviggiani@schema31.it>
2020-10-23 15:26:03 +0200

    modifiche per wildfly

--- src/main/java/it/javaboss/MainApp.java
+++ src/main/java/it/javaboss/MainApp.java
@@ -1,9 +1,10 @@
 package it.javaboss;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainApp {
+public class MainApp extends SpringBootServletInitializer{
    public static void main(String[] args) {
       
 	   SpringApplication.run(MainApp.class, args);
